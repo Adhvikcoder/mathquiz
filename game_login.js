ab39f58e70a05b9506d4629cacc1af3df8fb06fa
Adhvikcoder <81934030+Adhvikcoder@users.noreply.github.com>
2021-09-23 15:37:44 -0400

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,8 @@
+function adduser(){
+    user_name1 = document.getElementById("player1_input").value;
+    user_name2 = document.getElementById("player2_input").value;
+localStorage.setItem("user_name1",user_name1);
+localStorage.setItem("user_name2",user_name2);
+
+window.location="game_quiz.html";
+}
